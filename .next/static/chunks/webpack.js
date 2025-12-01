67d517af9ef1943135ddb50c0f470a0fdb6885e5
sharathhy <sharath.hy@c5i.ai>
2025-12-01 15:33:03 +0530

    package.json change

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2572f51e197ef64"; }
+/******/ 		__webpack_require__.h = function() { return "c87fa52331f95d7f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
