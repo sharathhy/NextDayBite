64250b845d93c1914aa617dd959ce1b819126aef
sharathhy <sharath.hy@c5i.ai>
2025-12-01 12:47:21 +0530

    config changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e6b96d7be488e5c8"; }
+/******/ 		__webpack_require__.h = function() { return "d2572f51e197ef64"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
