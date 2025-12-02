f00f66a4a4ddb694e6dcb7f08996ae2bfc6783c1
sharathhy <sharath.hy@c5i.ai>
2025-12-02 18:45:05 +0530

    test

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const { createServer } = require("http");
 const next = require("next");
 
+const port = process.env.PORT || 3000;
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => handle(req, res)).listen(process.env.PORT || 3000);
+  createServer((req, res) => handle(req, res)).listen(port, () => {
+    console.log("Server running on port", port);
+  });
 });
