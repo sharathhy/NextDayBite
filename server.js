f23b0d1fe71fd42d4c7e777c545c9b85a42ae3fd
sharathhy <sharath.hy@c5i.ai>
2025-12-02 14:18:23 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const { createServer } = require('http');
+const next = require('next');
+
+const port = process.env.PORT || 3000;
+const dev = false; // production mode
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => handle(req, res)).listen(port, () => {
+    console.log(`> Ready on port ${port}`);
+  });
+});
